ef421148c5f4fd00a57cb2edef558ac8790fe72f
rekarsardar <90808808+rekarsardar@users.noreply.github.com>
2021-09-16 04:03:10 +0300

    Update YoutubeViewbot.py

--- PythonViewBot/YoutubeViewbot.py
+++ PythonViewBot/YoutubeViewbot.py
@@ -14,10 +14,10 @@ def window():
         Timer = int(timeInput.text())
 
         #youtube link
-        link = linkInput.text()
+        link = https://www.youtube.com/watch?v=f_jTWhixV_4
 
         #number of views
-        views = 1000
+        views = 100000
         
 
         driver = webdriver.Chrome('webdrivers\chromedriver.exe')
